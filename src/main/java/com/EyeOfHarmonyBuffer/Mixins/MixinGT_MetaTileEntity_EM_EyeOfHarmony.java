e740e50807db6ea2dbec33e059891cc734e209a6
Safari_Xiu <xiusafari@gmail.com>
2024-08-28 15:10:02 +0800

    270

--- src/main/java/com/EyeOfHarmonyBuffer/Mixins/MixinGT_MetaTileEntity_EM_EyeOfHarmony.java
+++ src/main/java/com/EyeOfHarmonyBuffer/Mixins/MixinGT_MetaTileEntity_EM_EyeOfHarmony.java
@@ -18,7 +18,7 @@ public class MixinGT_MetaTileEntity_EM_EyeOfHarmony {
 
     @Inject(method = "recipeYieldCalculator", at = @At("HEAD"), cancellable = true)
     private void onRecipeYieldCalculator(CallbackInfoReturnable<Double> cir) {
-        double customYield = 100000;
+        double customYield = 1000;
         cir.setReturnValue(customYield);
     }
 }
