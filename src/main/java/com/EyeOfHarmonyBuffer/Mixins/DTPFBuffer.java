121123d3fe2a47af30d082b9c3f85bb8b05133d7
Safari_Xiu <xiusafari@gmail.com>
2024-09-13 11:18:18 +0800

    超维度锻炉减免第一次修改

--- src/main/java/com/EyeOfHarmonyBuffer/Mixins/DTPFBuffer.java
+++ src/main/java/com/EyeOfHarmonyBuffer/Mixins/DTPFBuffer.java
@@ -1,12 +1,12 @@
 package com.EyeOfHarmonyBuffer.Mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
-
-import gregtech.common.tileentities.machines.multi.GT_MetaTileEntity_PlasmaForge;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import gregtech.common.tileentities.machines.multi.GT_MetaTileEntity_PlasmaForge;
 
 @Mixin(value = GT_MetaTileEntity_PlasmaForge.class, remap = false)
 public abstract class DTPFBuffer {
@@ -15,7 +15,7 @@ public abstract class DTPFBuffer {
     private double discount;
 
     @Inject(method = "recalculateDiscount", at = @At("HEAD"), cancellable = true)
-    private void LockDiscount(CallbackInfo ci){
+    private void LockDiscount(CallbackInfo ci) {
         this.discount = 1.0;
 
         ci.cancel();
