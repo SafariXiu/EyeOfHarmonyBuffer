ad0152e06ba8363be227e2bae0d3d492cd0ea8cb
Safari_Xiu <xiusafari@gmail.com>
2024-11-06 23:34:57 +0800

    修复聚变添加mixin 现在他们可以正常使用了

--- src/main/java/com/EyeOfHarmonyBuffer/Mixins/LargeFusionMixin.java
+++ src/main/java/com/EyeOfHarmonyBuffer/Mixins/LargeFusionMixin.java
@@ -1,9 +1,7 @@
 package com.EyeOfHarmonyBuffer.Mixins;
 
-import java.lang.reflect.Field;
-import java.util.List;
-
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -11,62 +9,30 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import com.EyeOfHarmonyBuffer.Config.MainConfig;
 
 import goodgenerator.blocks.tileEntity.base.MTELargeFusionComputer;
-import gregtech.api.metatileentity.implementations.MTEHatchEnergy;
-import tectech.thing.metaTileEntity.hatch.MTEHatchEnergyMulti;
+import gregtech.api.enums.GTValues;
 
 @Mixin(value = MTELargeFusionComputer.class, remap = false)
 public class LargeFusionMixin {
 
-    @Inject(method = "getSingleHatchPower", at = @At("RETURN"), cancellable = true)
-    private void modifyGetSingleHatchPower(CallbackInfoReturnable<Long> cir) {
-        if (MainConfig.LargeFusionMixin) {
-            MTELargeFusionComputer fusionComputer = (MTELargeFusionComputer) (Object) this;
+    /**
+     * 覆盖 getSingleHatchPower 方法
+     * 去掉 extraPara(100) 和 / 32，保持功率计算为 电压 * 安培数。
+     *
+     * @author EyeOfHarmonyBuffer
+     * @reason Simplify the power calculation by removing extraPara(100) and /32 to match current design.
+     */
+    @Overwrite
+    protected long getSingleHatchPower() {
+        MTELargeFusionComputer fusionComputer = (MTELargeFusionComputer) (Object) this;
 
-            List<MTEHatchEnergy> energyHatches = fusionComputer.mEnergyHatches;
-
-            List<MTEHatchEnergyMulti> energyMultiHatches = getEnergyMultiHatches(fusionComputer);
-
-            long totalPower = 0;
-
-            for (MTEHatchEnergy energyHatch : energyHatches) {
-                long amperage = energyHatch.maxAmperesOut();
-                long voltage = energyHatch.getBaseMetaTileEntity()
-                    .getInputVoltage();
-                totalPower += amperage * voltage;
-            }
-
-            if (energyMultiHatches != null) {
-                for (MTEHatchEnergyMulti energyMultiHatch : energyMultiHatches) {
-                    long amperage = energyMultiHatch.maxAmperesOut();
-                    long voltage = energyMultiHatch.getBaseMetaTileEntity()
-                        .getInputVoltage();
-                    totalPower += amperage * voltage;
-                }
-            }
-
-            cir.setReturnValue(totalPower);
-        }
+        return GTValues.V[fusionComputer.tier()] * fusionComputer.getMaxPara() * 32;
     }
 
     @Inject(method = "maxEUStore", at = @At("RETURN"), cancellable = true)
     private void modifyMaxEUStore(CallbackInfoReturnable<Long> cir) {
         if (MainConfig.LargeFusionMixin) {
-            MTELargeFusionComputer fusionComputer = (MTELargeFusionComputer) (Object) this;
-            long maxEnergyStore = fusionComputer.capableStartupCanonical() * 32L / 32L;
+            long maxEnergyStore = 10000000000000L;
             cir.setReturnValue(maxEnergyStore);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private List<MTEHatchEnergyMulti> getEnergyMultiHatches(MTELargeFusionComputer fusionComputer) {
-        try {
-            Field energyMultiField = MTELargeFusionComputer.class.getSuperclass()
-                .getDeclaredField("eEnergyMulti");
-            energyMultiField.setAccessible(true);
-            return (List<MTEHatchEnergyMulti>) energyMultiField.get(fusionComputer);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
-            return null;
         }
     }
 }
---
+++ src/main/java/com/EyeOfHarmonyBuffer/Mixins/TTMultiblockBaseAccessor.java
@@ -0,0 +1,17 @@
+package com.EyeOfHarmonyBuffer.Mixins;
+
+import java.util.ArrayList;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import tectech.thing.metaTileEntity.hatch.MTEHatchEnergyMulti;
+import tectech.thing.metaTileEntity.multi.base.TTMultiblockBase;
+
+@Mixin(value = TTMultiblockBase.class, remap = false)
+public interface TTMultiblockBaseAccessor {
+
+    @Accessor("eEnergyMulti")
+    ArrayList<MTEHatchEnergyMulti> getEnergyMulti();
+
+}
