58d4da2b77882a6baa0d144d67271e6272011669
Safari_Xiu <xiusafari@gmail.com>
2024-11-19 23:06:55 +0800

    删除部分作用不大的mixin只保留 采矿模块并行修改，等离子体不消耗与锁定工作时间

--- src/main/java/com/EyeOfHarmonyBuffer/utils/LateMixins.java
+++ src/main/java/com/EyeOfHarmonyBuffer/utils/LateMixins.java
@@ -44,10 +44,7 @@ public class LateMixins implements ILateMixinLoader {
             "EOH.EyeOfHarmonyZeroPowerStart",
             "LargeFusion.LargeFusionMixin",
             "LargeFusion.LargeFusionPara",
-            "SpaceElevator.ModuleAssemblerParallelsMixin",
-            "SpaceElevator.ModuleAssemblerTicksMixin",
             "SpaceElevator.ModuleMinerMixin",
-            "SpaceElevator.ModuleAssemblerTicksMixin"
-        );
+            "SpaceElevator.ModuleMinerParallelsMixin");
     }
 }
