65ced2c52836da1197d5e1df9c2d3d9e9b0862cd
Safari_Xiu <xiusafari@gmail.com>
2024-12-10 20:36:10 +0800

    修复NEI中结构破碎的问题，并添加对输入仓数量的修改限制，编写配置文件

--- src/main/java/com/EyeOfHarmonyBuffer/Config/MainConfig.java
+++ src/main/java/com/EyeOfHarmonyBuffer/Config/MainConfig.java
@@ -10,6 +10,7 @@ public class MainConfig {
     public static String constantOutputEUConfig = "3812901725648391027364519283746501928374652019384756209183475620193847562019384756201938475620193847562";
     public static boolean GasInPut = true;
     public static boolean EOHinputBusMe = true;
+    public static boolean EOHinputHatchEnable= true;
     public static boolean enableFluidOutPut = true;
     public static boolean EOHItemInPut = true;
     public static int EOHtime = 128;
@@ -107,6 +108,10 @@ public class MainConfig {
         EOHinputBusMe = config
             .get("鸿蒙之眼功能", "鸿蒙之眼ME输入总线", EOHinputBusMe, "启用鸿蒙之眼ME输入总线")
             .getBoolean(EOHinputBusMe);
+
+        EOHinputHatchEnable = config
+            .get("鸿蒙之眼功能","鸿蒙之眼输入仓",EOHinputHatchEnable,"开启后鸿蒙之眼不强制需求2个输入仓，可以为0,1,2个")
+            .getBoolean(EOHinputHatchEnable);
 
         enableFluidOutPut = config
             .get("鸿蒙之眼功能", "鸿蒙之眼额外流体产出", enableFluidOutPut, "鸿蒙之眼额外流体产出是否启用")
--- src/main/java/com/EyeOfHarmonyBuffer/Mixins/EOH/EyeOfHarmonyBus.java
+++ src/main/java/com/EyeOfHarmonyBuffer/Mixins/EOH/EyeOfHarmonyBus.java
@@ -1,27 +1,101 @@
 package com.EyeOfHarmonyBuffer.Mixins.EOH;
 
-import java.util.List;
-
+import com.gtnewhorizon.structurelib.alignment.constructable.IConstructable;
+import com.gtnewhorizon.structurelib.alignment.constructable.ISurvivalConstructable;
+import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
+import gregtech.common.tileentities.machines.MTEHatchInputBusME;
+import gregtech.common.tileentities.machines.MTEHatchOutputBusME;
+import gregtech.common.tileentities.machines.MTEHatchOutputME;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
 import com.EyeOfHarmonyBuffer.Config.MainConfig;
 
 import tectech.thing.metaTileEntity.multi.MTEEyeOfHarmony;
+import tectech.thing.metaTileEntity.multi.base.TTMultiblockBase;
+
+import static gregtech.common.tileentities.machines.multi.MTEFusionComputer.STRUCTURE_PIECE_MAIN;
 
 @Mixin(value = MTEEyeOfHarmony.class, remap = false)
-public class EyeOfHarmonyBus {
+public abstract class EyeOfHarmonyBus extends TTMultiblockBase implements IConstructable, ISurvivalConstructable {
 
-    @Inject(method = "checkMachine_EM", at = @At(value = "RETURN"), cancellable = true)
-    private void injectModifyReturn(CallbackInfoReturnable<Boolean> cir) {
-        List<?> mInputBusses = ((MTEEyeOfHarmony) (Object) this).mInputBusses;
+    @Shadow
+    private int stabilisationFieldMetadata;
 
-        if (MainConfig.EOHinputBusMe) {
-            if (mInputBusses != null && !mInputBusses.isEmpty()) {
-                cir.setReturnValue(true);
+    @Shadow
+    private int timeAccelerationFieldMetadata;
+
+    @Shadow
+    private int spacetimeCompressionFieldMetadata;
+
+    protected EyeOfHarmonyBus(int aID, String aName, String aNameRegional) {
+        super(aID, aName, aNameRegional);
+    }
+
+    /**
+     * @author EyeOfHarmonyBuffer
+     * @reason 覆写EOH的舱室检测逻辑
+     */
+    @Overwrite
+    public boolean checkMachine_EM(IGregTechTileEntity iGregTechTileEntity, ItemStack itemStack) {
+
+        spacetimeCompressionFieldMetadata = -1;
+        timeAccelerationFieldMetadata = -1;
+        stabilisationFieldMetadata = -1;
+
+        if (!structureCheck_EM(STRUCTURE_PIECE_MAIN, 16, 16, 0)) {
+            return false;
+        }
+
+        if (!mDualInputHatches.isEmpty()) {
+            return false;
+        }
+
+        {
+            if (mOutputBusses.size() != 1) {
+                return false;
+            }
+
+            if (!(mOutputBusses.get(0) instanceof MTEHatchOutputBusME)) {
+                return false;
             }
         }
+
+        {
+            if (mOutputHatches.size() != 1) {
+                return false;
+            }
+
+            if (!(mOutputHatches.get(0) instanceof MTEHatchOutputME)) {
+                return false;
+            }
+        }
+
+        {
+            if (mInputBusses.size() != 1) {
+                return false;
+            }
+
+            if (mInputBusses.get(0) instanceof MTEHatchInputBusME) {
+                return MainConfig.EOHinputBusMe;
+            }
+        }
+
+        {
+            if (!mEnergyHatches.isEmpty()) {
+                return false;
+            }
+
+            if (!mExoticEnergyHatches.isEmpty()) {
+                return false;
+            }
+        }
+        if(MainConfig.EOHinputHatchEnable){
+            return mInputHatches.size() <= 2;
+        }
+        return mInputHatches.size() == 2;
+
     }
 }
