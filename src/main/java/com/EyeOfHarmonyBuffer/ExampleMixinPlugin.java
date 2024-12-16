8f1f42dfdfee733de3949fdd312338705dbdb6ac
Safari_Xiu <xiusafari@gmail.com>
2024-12-16 21:28:24 +0800

    添加针对额外MOD的动态加载机制

---
+++ src/main/java/com/EyeOfHarmonyBuffer/ExampleMixinPlugin.java
@@ -0,0 +1,55 @@
+package com.EyeOfHarmonyBuffer;
+
+import org.spongepowered.asm.lib.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class ExampleMixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+        System.out.println("[ExampleMixinPlugin] Loaded for package: " + mixinPackage);
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (mixinClassName.equals("com.example.mixin.IndistinctTentaclePrototypeMK2Mixin")) {
+            try {
+                Class.forName("com.Nxer.TwistSpaceTechnology.common.modularizedMachine.MM_DimensionallyTranscendentMatterPlasmaForgePrototypeMK2", false, this.getClass().getClassLoader());
+                System.out.println("[ExampleMixinPlugin] Target class found, applying Mixin: " + mixinClassName);
+                return true;
+            } catch (ClassNotFoundException e) {
+                System.out.println("[ExampleMixinPlugin] Target class not found, skipping Mixin: " + mixinClassName);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {
+
+    }
+}
