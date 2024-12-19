68c0298ed101f4139c152950057d4b871847a45a
Safari_Xiu <xiusafari@gmail.com>
2024-12-20 01:36:14 +0800

    太阳聚变异化器Mixin第二部分

--- src/main/java/com/EyeOfHarmonyBuffer/utils/PredeterminedValues.java
+++ src/main/java/com/EyeOfHarmonyBuffer/utils/PredeterminedValues.java
@@ -2,29 +2,20 @@ package com.EyeOfHarmonyBuffer.utils;
 
 public class PredeterminedValues {
 
-    public static final int[] FLUID_WEIGHTS = new int[]{1, 2, 3, 4, 5, 6, 7};
-    public static final int[] ITEM_WEIGHTS = new int[]{1, 3, 5, 7, 9, 11, 13};
-    private static int fluidIndex = 0;
-    private static int itemIndex = 0;
+    // 简化为单一权重
+    public static final int FLUID_WEIGHT = 3; // 固定的流体权重
+    public static final int ITEM_WEIGHT = 3; // 固定的物品权重
 
+    // 获取流体权重
     public static int getNextFluidWeight() {
-        if (FLUID_WEIGHTS.length == 0) {
-            throw new IllegalStateException("FLUID_WEIGHTS array is empty!");
-        }
-        int weight = FLUID_WEIGHTS[fluidIndex];
-        fluidIndex = (fluidIndex + 1) % FLUID_WEIGHTS.length;
-        System.out.println("Selected Fluid Weight: " + weight);
-        return weight;
+        System.out.println("Selected Fluid Weight: " + FLUID_WEIGHT);
+        return FLUID_WEIGHT;
     }
 
+    // 获取物品权重
     public static int getNextItemWeight() {
-        if (ITEM_WEIGHTS.length == 0) {
-            throw new IllegalStateException("ITEM_WEIGHTS array is empty!");
-        }
-        int weight = ITEM_WEIGHTS[itemIndex];
-        itemIndex = (itemIndex + 1) % ITEM_WEIGHTS.length;
-        System.out.println("Selected Item Weight: " + weight);
-        return weight;
+        System.out.println("Selected Item Weight: " + ITEM_WEIGHT);
+        return ITEM_WEIGHT;
     }
 
 }
