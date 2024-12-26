66d6895987a15cf0ef0b9aa8004bad7008a88050
Safari_Xiu <xiusafari@gmail.com>
2024-12-26 17:57:27 +0800

    添加针对行星气体钻机的mixin

---
+++ src/main/java/com/EyeOfHarmonyBuffer/Mixins/PlanetaryGasSiphonMixin.java
@@ -0,0 +1,36 @@
+package com.EyeOfHarmonyBuffer.Mixins;
+
+import com.EyeOfHarmonyBuffer.Config.MainConfig;
+import com.gtnewhorizons.gtnhintergalactic.tile.multi.TileEntityPlanetaryGasSiphon;
+import gregtech.api.interfaces.IChunkLoader;
+import gregtech.api.metatileentity.implementations.MTEEnhancedMultiBlockBase;
+import gregtech.api.recipe.check.CheckRecipeResult;
+import net.minecraftforge.fluids.FluidStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = TileEntityPlanetaryGasSiphon.class,remap = false)
+public abstract class PlanetaryGasSiphonMixin extends MTEEnhancedMultiBlockBase<TileEntityPlanetaryGasSiphon>
+    implements IChunkLoader {
+
+    protected PlanetaryGasSiphonMixin(int aID, String aName, String aNameRegional) {
+        super(aID, aName, aNameRegional);
+    }
+
+    @Inject(method = "checkProcessing", at = @At("TAIL"))
+    private void modifyOutputFluid(CallbackInfoReturnable<CheckRecipeResult> cir) {
+        if(MainConfig.PlanetaryGasSiphonEnable){
+            TileEntityPlanetaryGasSiphon siphon = (TileEntityPlanetaryGasSiphon) (Object) this;
+
+            if (siphon.mOutputFluids != null && siphon.mOutputFluids.length > 0) {
+                for (FluidStack fluid : siphon.mOutputFluids) {
+                    if (fluid != null) {
+                        fluid.amount *= MainConfig.PlanetaryGasSiphonParallel;
+                    }
+                }
+            }
+        }
+    }
+}
