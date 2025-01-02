0d7c0151e16722afce2fe77d72c4a58ae26720fc
Safari_Xiu <xiusafari@gmail.com>
2025-01-02 13:30:19 +0800

    缓存反射的outputItemToAENetwork对象 提升代码性能

--- src/main/java/com/EyeOfHarmonyBuffer/Mixins/EOH/EyeOfHarmonyItemMixin.java
+++ src/main/java/com/EyeOfHarmonyBuffer/Mixins/EOH/EyeOfHarmonyItemMixin.java
@@ -19,9 +19,22 @@ import tectech.thing.metaTileEntity.multi.MTEEyeOfHarmony;
 @Mixin(value = MTEEyeOfHarmony.class, remap = false)
 public abstract class EyeOfHarmonyItemMixin {
 
+    // 缓存反射 Method 对象
+    private static Method cachedOutputMethod;
+
+    static {
+        try {
+            Class<?> clazz = MTEEyeOfHarmony.class;
+            cachedOutputMethod = clazz.getDeclaredMethod("outputItemToAENetwork", ItemStack.class, long.class);
+            cachedOutputMethod.setAccessible(true);
+        } catch (NoSuchMethodException e) {
+            System.err.println("无法找到 outputItemToAENetwork 方法，初始化失败！");
+            e.printStackTrace();
+        }
+    }
+
     @Inject(method = "outputAfterRecipe_EM", at = @At("TAIL"))
     private void injectCustomOutput(CallbackInfo ci) {
-
         try {
             if (!MainConfig.EOHItemInPut) {
                 return;
@@ -45,25 +58,24 @@ public abstract class EyeOfHarmonyItemMixin {
             System.err.println("injectCustomOutput 方法中发生异常：");
             e.printStackTrace();
         }
-
     }
 
     /**
      * 自定义方法，用于将带有 long 类型数量的物品注入到 AE 网络中。
-     * 这里假设 outputItemToAENetwork 方法可以接收 long 类型的物品数量。
+     * 使用缓存的反射 Method 对象来减少性能开销。
      *
      * @param customStack 自定义的 CustomItemStackLong 对象
      */
     private void outputLongToAENetwork(CustomItemStackLong customStack) {
         try {
-            Class<?> clazz = MTEEyeOfHarmony.class;
-            Method method = clazz.getDeclaredMethod("outputItemToAENetwork", ItemStack.class, long.class);
-            method.setAccessible(true);
+            if (cachedOutputMethod == null) {
+                throw new IllegalStateException("缓存的 outputItemToAENetwork 方法未初始化！");
+            }
 
             ItemStack itemStack = customStack.toItemStack();
-
-            method.invoke(this, itemStack, customStack.getQuantity());
+            cachedOutputMethod.invoke(this, itemStack, customStack.getQuantity());
         } catch (Exception e) {
+            System.err.println("outputLongToAENetwork 方法中发生异常：");
             e.printStackTrace();
         }
     }
