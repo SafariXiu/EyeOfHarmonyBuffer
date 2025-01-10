3dc98d70123cb36753565725092a7e669eba9ba4
Safari_Xiu <xiusafari@gmail.com>
2025-01-11 00:46:41 +0800

    添加HV变压器的配方

--- src/main/java/com/EyeOfHarmonyBuffer/Recipe/AssemblerRecipes.java
+++ src/main/java/com/EyeOfHarmonyBuffer/Recipe/AssemblerRecipes.java
@@ -15,6 +15,7 @@ public final class AssemblerRecipes implements IRecipePool {
     @Override
     public void loadRecipes() {
 
+        //2空间组件
         GTValues.RA.stdBuilder()
             .itemInputs(
                 getModItem(NewHorizonsCoreMod.ID,"item.EngineeringProcessorSpatialPulsatingCore",1,0),
@@ -28,6 +29,7 @@ public final class AssemblerRecipes implements IRecipePool {
             .duration(20 * SECONDS)
             .addTo(assemblerRecipes);
 
+        //16空间组件
         GTValues.RA.stdBuilder()
             .itemInputs(
                 getModItem(NewHorizonsCoreMod.ID,"item.EngineeringProcessorSpatialPulsatingCore",1,0),
@@ -41,6 +43,7 @@ public final class AssemblerRecipes implements IRecipePool {
             .duration(20 * SECONDS)
             .addTo(assemblerRecipes);
 
+        //128空间组件
         GTValues.RA.stdBuilder()
             .itemInputs(
                 getModItem(NewHorizonsCoreMod.ID,"item.EngineeringProcessorSpatialPulsatingCore",1,0),
@@ -54,6 +57,7 @@ public final class AssemblerRecipes implements IRecipePool {
             .duration(20 * SECONDS)
             .addTo(assemblerRecipes);
 
+        //福鲁伊克斯珍珠
         GTValues.RA.stdBuilder()
             .itemInputs(
                 GTOreDictUnificator.get(OrePrefixes.gem, Materials.EnderPearl,1),
@@ -67,6 +71,7 @@ public final class AssemblerRecipes implements IRecipePool {
             .duration(20 * SECONDS)
             .addTo(assemblerRecipes);
 
+        //福鲁伊克斯珍珠
         GTValues.RA.stdBuilder()
             .itemInputs(
                 GTOreDictUnificator.get(OrePrefixes.gem, Materials.EnderPearl,1),
@@ -78,6 +83,21 @@ public final class AssemblerRecipes implements IRecipePool {
             )
             .eut(TierEU.RECIPE_EV)
             .duration(20 * SECONDS)
+            .addTo(assemblerRecipes);
+
+        //HV变压器
+        GTValues.RA.stdBuilder()
+            .itemInputs(
+                ItemList.Hull_HV.get(1),
+                GTOreDictUnificator.get(OrePrefixes.cableGt01,Materials.Gold,4),
+                GTOreDictUnificator.get(OrePrefixes.cableGt01,Materials.Aluminium,1),
+                ItemList.Circuit_Chip_ULPIC.get(2)
+            )
+            .itemOutputs(
+                ItemList.Transformer_EV_HV.get(1)
+            )
+            .eut(TierEU.RECIPE_HV)
+            .duration(5 * SECONDS)
             .addTo(assemblerRecipes);
 
     }
