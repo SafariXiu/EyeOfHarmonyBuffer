db4f6f1aec29d1c1bb984493453a700087156313
Safari_Xiu <xiusafari@gmail.com>
2025-01-22 14:48:47 +0800

    修正语言文件并开始编写机器部分代码

--- src/main/java/com/EyeOfHarmonyBuffer/common/Machine/EOHB_VendingMachines.java
+++ src/main/java/com/EyeOfHarmonyBuffer/common/Machine/EOHB_VendingMachines.java
@@ -1,21 +1,38 @@
 package com.EyeOfHarmonyBuffer.common.Machine;
 
 import com.EyeOfHarmonyBuffer.common.multiMachineClasses.WirelessEnergyMultiMachineBase;
+import com.EyeOfHarmonyBuffer.common.multiMachineClasses.processingLogics.GTCM_ProcessingLogic;
 import com.gtnewhorizon.structurelib.structure.IStructureDefinition;
 import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
+import gregtech.api.interfaces.tileentity.IWirelessEnergyHatchInformation;
+import gregtech.api.logic.ProcessingLogic;
+import gregtech.api.util.GTRecipe;
 import gregtech.api.util.MultiblockTooltipBuilder;
+import gregtech.api.util.OverclockCalculator;
+import gregtech.api.util.ParallelHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.util.ForgeDirection;
+import org.jetbrains.annotations.NotNull;
 
-import static com.EyeOfHarmonyBuffer.utils.TextLocalization.Tooltip_VendingMachines_Controller;
-import static com.EyeOfHarmonyBuffer.utils.TextLocalization.Tooltip_VendingMachines_MachineType;
+import javax.annotation.Nonnull;
 
-public class EOHB_VendingMachines extends WirelessEnergyMultiMachineBase<EOHB_VendingMachines> {
+import static com.EyeOfHarmonyBuffer.utils.TextLocalization.*;
+
+public class EOHB_VendingMachines extends WirelessEnergyMultiMachineBase<EOHB_VendingMachines> implements IWirelessEnergyHatchInformation {
 
     public EOHB_VendingMachines(int aID, String aName, String aNameRegional) {
         super(aID, aName, aNameRegional);
+    }
+
+    public EOHB_VendingMachines(String aName) {
+        super(aName);
+    }
+
+    @Override
+    public IMetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
+        return new EOHB_VendingMachines(this.mName);
     }
 
     @Override
@@ -39,6 +56,39 @@ public class EOHB_VendingMachines extends WirelessEnergyMultiMachineBase<EOHB_Ve
     }
 
     @Override
+    protected ProcessingLogic createProcessingLogic(){
+        return new GTCM_ProcessingLogic(){
+
+            @Override
+            protected @NotNull OverclockCalculator createOverclockCalculator(@Nonnull GTRecipe recipe) {
+                return OverclockCalculator.ofNoOverclock(recipe);
+            }
+
+            @NotNull
+            @Override
+            protected ParallelHelper createParallelHelper(@NotNull GTRecipe recipe) {
+                return new ParallelHelper()
+                    .setRecipe(recipe)
+                    .setItemInputs(inputItems)
+                    .setFluidInputs(inputFluids)
+                    .setAvailableEUt(Integer.MAX_VALUE) // 设置无限能量
+                    .setMachine(machine, protectItems, protectFluids)
+                    .setMaxParallel(Integer.MAX_VALUE) // 设置极大并行
+                    .setEUtModifier(0.0) // 不耗电
+                    .enableBatchMode(batchSize) // 启用批量模式
+                    .setConsumption(true)
+                    .setOutputCalculation(true);
+            }
+
+            @Override
+            protected double calculateDuration(@Nonnull GTRecipe recipe, @Nonnull ParallelHelper helper,
+                                               @Nonnull OverclockCalculator calculator) {
+                return 128;
+            }
+        }.setMaxParallel(Integer.MAX_VALUE);
+    }
+
+    @Override
     public void construct(ItemStack stackSize, boolean hintsOnly) {
 
     }
@@ -52,18 +102,15 @@ public class EOHB_VendingMachines extends WirelessEnergyMultiMachineBase<EOHB_Ve
     protected MultiblockTooltipBuilder createTooltip() {
         final MultiblockTooltipBuilder tt = new MultiblockTooltipBuilder();
         tt.addMachineType(Tooltip_VendingMachines_MachineType)
-            .addInfo(Tooltip_VendingMachines_Controller);
+            .addInfo(Tooltip_VendingMachines_Controller)
+            .addInfo(Tooltip_VendingMachines_00)
+            .addInfo(Tooltip_VendingMachines_01);
         return tt;
     }
 
     @Override
     public boolean checkMachine(IGregTechTileEntity aBaseMetaTileEntity, ItemStack aStack) {
         return false;
-    }
-
-    @Override
-    public IMetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
-        return null;
     }
 
     @Override
--- src/main/java/com/EyeOfHarmonyBuffer/utils/TextLocalization.java
+++ src/main/java/com/EyeOfHarmonyBuffer/utils/TextLocalization.java
@@ -4,19 +4,26 @@ public class TextLocalization {
     public static final String Waila_WirelessMode;
     public static final String Waila_CurrentEuCost;
     public static final String NameVendingMachines;
+    public static final String WirelessNetwork;
     public static final String Tooltip_VendingMachines_MachineType;
     public static final String Tooltip_VendingMachines_Controller;
+    public static final String Tooltip_VendingMachines_00;
+    public static final String Tooltip_VendingMachines_01;
 
     public TextLocalization() {
     }
 
     static{
+        //默认
         Waila_WirelessMode = TextHandler.texter("Wireless Mode", "Waila.General.WirelessMode");
         Waila_CurrentEuCost = TextHandler.texter("Current EU Cost", "Waila.General.CurrentEuCost");
+        WirelessNetwork = TextHandler.texter("Directly get EU from the Wireless EU Net.", "WirelessNetwork");
 
         //大型贸易机
         NameVendingMachines = TextHandler.texter("Vending Machines", "NameVendingMachines");
         Tooltip_VendingMachines_MachineType = TextHandler.texter("Vending Machines", "Tooltip_VendingMachines_MachineType");
         Tooltip_VendingMachines_Controller = TextHandler.texter("Controller block for the Vending Machines", "Tooltip_VendingMachines_Controller");
+        Tooltip_VendingMachines_00 = TextHandler.texter("A large trade machine, Vending Machines, marks items in the Crafting Input Buffer (ME) NC slot to set the machine's output items.", "Tooltip_VendingMachines_00");
+        Tooltip_VendingMachines_01 = TextHandler.texter("When powered by a wireless EU Net, it supports parallel multi-recipe processing with a fixed working time of 6.4 seconds.", "Tooltip_VendingMachines_01");
     }
 }
