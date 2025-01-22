45298d952950089c53102afc61839069ff9d50f4
Safari_Xiu <xiusafari@gmail.com>
2025-01-23 05:09:15 +0800

    为细菌培养缸添加全新Mixin并重构代码

--- src/main/java/com/EyeOfHarmonyBuffer/Mixins/BioVatMixin.java
+++ src/main/java/com/EyeOfHarmonyBuffer/Mixins/BioVatMixin.java
@@ -1,25 +1,81 @@
 package com.EyeOfHarmonyBuffer.Mixins;
 
+import com.EyeOfHarmonyBuffer.Config.MainConfig;
+import com.gtnewhorizon.structurelib.alignment.constructable.ISurvivalConstructable;
+import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
+import gregtech.api.logic.ProcessingLogic;
+import gregtech.api.metatileentity.implementations.MTEEnhancedMultiBlockBase;
+import gregtech.api.recipe.check.CheckRecipeResult;
+import gregtech.api.recipe.check.CheckRecipeResultRegistry;
+import gregtech.api.util.GTRecipe;
+import gregtech.api.util.ParallelHelper;
+import net.minecraftforge.fluids.FluidStack;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import com.EyeOfHarmonyBuffer.Config.MainConfig;
-
-import bartworks.common.configs.Configuration;
 import bartworks.common.tileentities.multis.MTEBioVat;
 
+import javax.annotation.Nonnull;
+
 @Mixin(value = MTEBioVat.class, remap = false)
-public class BioVatMixin {
+public abstract class BioVatMixin extends MTEEnhancedMultiBlockBase<MTEBioVat> implements ISurvivalConstructable {
 
-    @Inject(method = "calcMod", at = @At("RETURN"), cancellable = true)
-    private void injectCalcMod(double x, CallbackInfoReturnable<Integer> cir) {
+    @Shadow
+    private int mSievert;
 
-        if (MainConfig.BioVatTrue) {
-            double z = Configuration.Multiblocks.bioVatMaxParallelBonus;
+    @Shadow
+    protected abstract GTRecipe recipeWithMultiplier(GTRecipe recipe, FluidStack[] fluidInputs);
 
-            cir.setReturnValue((int) z);
+    protected BioVatMixin(int aID, String aName, String aNameRegional) {
+        super(aID, aName, aNameRegional);
+    }
+
+    @Inject(method = "createProcessingLogic", at = @At("RETURN"), cancellable = true)
+    private void modifyProcessingLogic(CallbackInfoReturnable<ProcessingLogic> cir) {
+        if(MainConfig.BioVatRadiationEnabled){
+            ProcessingLogic originalLogic = cir.getReturnValue();
+
+            ProcessingLogic wrappedLogic = new ProcessingLogic() {
+                @NotNull
+                @Override
+                protected CheckRecipeResult validateRecipe(@NotNull GTRecipe recipe) {
+                    return CheckRecipeResultRegistry.SUCCESSFUL;
+                }
+
+                @NotNull
+                @Override
+                protected ParallelHelper createParallelHelper(@Nonnull GTRecipe recipe) {
+                    return super.createParallelHelper(recipeWithMultiplier(recipe, inputFluids));
+                }
+            };
+
+            cir.setReturnValue(wrappedLogic);
+        }
+    }
+
+    @Inject(method = "recipeWithMultiplier", at = @At("HEAD"), cancellable = true)
+    private void modifyRecipeWithMultiplier(GTRecipe recipe, FluidStack[] fluidInputs, CallbackInfoReturnable<GTRecipe> cir) {
+        if (MainConfig.BioVatOutputRatioEnable){
+            GTRecipe tRecipe = recipe.copy();
+
+            int multiplier = MainConfig.BioVatOutputRatio;
+
+            if (tRecipe.mFluidOutputs != null && tRecipe.mFluidOutputs.length > 0) {
+                tRecipe.mFluidOutputs[0].amount *= multiplier;
+            }
+
+            cir.setReturnValue(tRecipe);
+        }
+    }
+
+    @Inject(method = "onPostTick", at = @At("HEAD"))
+    private void modifyOnPostTick(IGregTechTileEntity aBaseMetaTileEntity, long aTick, CallbackInfo ci) {
+        if(MainConfig.BioVatRadiationEnabled){
+            this.mSievert = Integer.MAX_VALUE;
         }
     }
 }
