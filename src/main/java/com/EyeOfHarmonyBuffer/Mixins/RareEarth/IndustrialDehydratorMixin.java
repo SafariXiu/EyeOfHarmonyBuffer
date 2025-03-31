8d522edf969d1c4101d5fedcf284a7578333ed62
William_Xiu <yuminghe21@163.com>
2025-03-31 12:51:29 +0800

    修复稀土线mixin

--- src/main/java/com/EyeOfHarmonyBuffer/Mixins/RareEarth/IndustrialDehydratorMixin.java
+++ src/main/java/com/EyeOfHarmonyBuffer/Mixins/RareEarth/IndustrialDehydratorMixin.java
@@ -8,6 +8,7 @@ import gregtech.api.recipe.check.CheckRecipeResult;
 import gregtech.api.recipe.check.CheckRecipeResultRegistry;
 import gregtech.api.util.GTRecipe;
 import gregtech.api.util.OverclockCalculator;
+import gregtech.api.util.ParallelHelper;
 import gtPlusPlus.xmod.gregtech.api.metatileentity.implementations.base.GTPPMultiBlockBase;
 import gtPlusPlus.xmod.gregtech.common.tileentities.machines.multi.processing.MTEIndustrialDehydrator;
 import org.jetbrains.annotations.NotNull;
@@ -16,6 +17,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import javax.annotation.Nonnull;
 
 @Mixin(value = MTEIndustrialDehydrator.class, remap = false)
 public abstract class IndustrialDehydratorMixin extends GTPPMultiBlockBase<MTEIndustrialDehydrator>
@@ -47,11 +50,37 @@ public abstract class IndustrialDehydratorMixin extends GTPPMultiBlockBase<MTEIn
                 @NotNull
                 @Override
                 protected OverclockCalculator createOverclockCalculator(@NotNull GTRecipe recipe) {
-                    return super.createOverclockCalculator(recipe)
-                        .setHeatOC(true)
-                        .setHeatDiscount(true)
-                        .setRecipeHeat(recipe.mSpecialValue)
-                        .setMachineHeat((int) getCoilLevel().getHeat());
+                    return new OverclockCalculator()
+                        //.setSpeedBoost(100.0) // 速度提升 100 倍
+                        .setParallel(Integer.MAX_VALUE) // 最大并行数
+                        .setEUt(0); // 不耗电
+                }
+
+                @NotNull
+                @Override
+                protected ParallelHelper createParallelHelper(@NotNull GTRecipe recipe) {
+                    return new ParallelHelper()
+                        .setRecipe(recipe)
+                        .setItemInputs(inputItems)
+                        .setFluidInputs(inputFluids)
+                        .setAvailableEUt(Integer.MAX_VALUE) // 设置无限能量
+                        .setMachine(machine, protectItems, protectFluids)
+                        .setMaxParallel(Integer.MAX_VALUE) // 设置极大并行
+                        .setEUtModifier(0.0) // 不耗电
+                        .enableBatchMode(batchSize) // 启用批量模式
+                        .setConsumption(true)
+                        .setOutputCalculation(true);
+                }
+
+                @Override
+                protected double calculateDuration(@Nonnull GTRecipe recipe, @Nonnull ParallelHelper helper,
+                                                   @Nonnull OverclockCalculator calculator) {
+                    return 10;
+                }
+
+                @Override
+                public CheckRecipeResult process() {
+                    return super.process();
                 }
             };
 
