d5e82d7ebb86864b8695d2428284c37a1bdb29a6
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-05-27 19:09:38 +0800

    fix gtpp machine can not use exoEnergyHatch issue

---
+++ src/main/java/com/EyeOfHarmonyBuffer/Mixins/GTPPMultiBlockBaseMixin.java
@@ -0,0 +1,36 @@
+package com.EyeOfHarmonyBuffer.Mixins;
+
+import gregtech.api.metatileentity.implementations.MTEExtendedPowerMultiBlockBase;
+import gregtech.api.metatileentity.implementations.MTEHatch;
+import gregtech.api.util.ExoticEnergyInputHelper;
+import gtPlusPlus.xmod.gregtech.api.metatileentity.implementations.base.GTPPMultiBlockBase;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static gregtech.api.util.GTUtility.filterValidMTEs;
+
+@SuppressWarnings("UnusedMixin")
+@Mixin(value = GTPPMultiBlockBase.class, remap = false)
+public abstract class GTPPMultiBlockBaseMixin<T extends MTEExtendedPowerMultiBlockBase<T>>
+    extends MTEExtendedPowerMultiBlockBase<T> {
+
+    @Shadow
+    public ArrayList<MTEHatch> mAllEnergyHatches;
+
+    protected GTPPMultiBlockBaseMixin(int aID, String aName, String aNameRegional) {
+        super(aID, aName, aNameRegional);
+    }
+
+    @Override
+    public List<MTEHatch> getExoticAndNormalEnergyHatchList() {
+        return new ArrayList<>(filterValidMTEs(this.mAllEnergyHatches));
+    }
+
+    @Override
+    public long getMaxInputVoltage() {
+        return ExoticEnergyInputHelper.getMaxInputVoltageMulti(getExoticAndNormalEnergyHatchList());
+    }
+}
