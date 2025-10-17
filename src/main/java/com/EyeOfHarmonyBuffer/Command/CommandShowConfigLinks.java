e5f61491559542d879febc533612910937266021
William_Xiu <yuminghe21@163.com>
2025-10-17 14:54:14 +0800

    删除Show指令中一些没用的文本

--- src/main/java/com/EyeOfHarmonyBuffer/Command/CommandShowConfigLinks.java
+++ src/main/java/com/EyeOfHarmonyBuffer/Command/CommandShowConfigLinks.java
@@ -36,25 +36,13 @@ public class CommandShowConfigLinks implements ICommand {
 
     @Override
     public void processCommand(ICommandSender sender, String[] args) {
-        if (!(sender instanceof EntityPlayer)) return;
-        EntityPlayer player = (EntityPlayer) sender;
         Minecraft mc = Minecraft.getMinecraft();
-
-        player.addChatMessage(new ChatComponentText(EOHB_Text_SeparatingLine));
-        player.addChatMessage(new ChatComponentText(EOHB_Client_PlayerJoin00));
-        player.addChatMessage(new ChatComponentText(EOHB_Client_PlayerJoin01));
-        player.addChatMessage(new ChatComponentText(EOHB_Client_PlayerJoin01_1));
 
         EOHBChatUtils.sendConfigLink(mc, EOHB_Client_PlayerJoin02, "/openEOHBconfig main.cfg");
         EOHBChatUtils.sendConfigLink(mc, EOHB_Client_PlayerJoin03, "/openEOHBconfig items.cfg");
         EOHBChatUtils.sendConfigLink(mc, EOHB_Client_PlayerJoin04, "/openEOHBconfig fluids.cfg");
         EOHBChatUtils.sendConfigLink(mc, EOHB_Client_PlayerJoin05, "/openEOHBconfig MachineLoaderConfig.cfg");
         EOHBChatUtils.sendConfigLink(mc, EOHB_Client_PlayerJoin06, "/openEOHBconfig folder");
-
-        player.addChatMessage(new ChatComponentText(EOHB_Client_PlayerJoin07));
-        player.addChatMessage(new ChatComponentText(EOHB_Client_PlayerJoin01_1));
-        player.addChatMessage(new ChatComponentText(EOHB_Client_PlayerJoin07_2));
-        player.addChatMessage(new ChatComponentText(EOHB_Text_SeparatingLine));
     }
 
     @Override
