42e5bd9e1b14823aee8eb23566e7f63e0ef39f04
Safari_Xiu <xiusafari@gmail.com>
2025-11-06 04:03:32 +0800

    为新的鸿蒙粉与锭添加独特配方

--- src/main/java/com/EyeOfHarmonyBuffer/Recipe/MaterialsRecipes.java
+++ src/main/java/com/EyeOfHarmonyBuffer/Recipe/MaterialsRecipes.java
@@ -1,15 +1,19 @@
 package com.EyeOfHarmonyBuffer.Recipe;
 
 import com.EyeOfHarmonyBuffer.utils.IRecipePool;
+import com.dreammaster.recipes.CustomItem;
 import gregtech.api.enums.*;
 import gregtech.api.util.GTOreDictUnificator;
 import gregtech.api.util.GTUtility;
+import tectech.thing.CustomItemList;
 
 import static com.EyeOfHarmonyBuffer.common.material.EOHBMaterialPool.EOHBCatalyst;
+import static gregtech.api.enums.MetaTileEntityIDs.EyeofHarmony;
 import static gregtech.api.enums.Mods.EternalSingularity;
 import static gregtech.api.util.GTModHandler.getModItem;
 import static gregtech.api.util.GTRecipeBuilder.SECONDS;
 import static gregtech.api.util.GTRecipeConstants.COIL_HEAT;
+import static gtPlusPlus.api.recipe.GTPPRecipeMaps.mixerNonCellRecipes;
 import static tectech.recipe.TecTechRecipeMaps.godforgeMoltenRecipes;
 
 public class MaterialsRecipes implements IRecipePool{
@@ -34,5 +38,25 @@ public class MaterialsRecipes implements IRecipePool{
             .duration(300 * SECONDS)
             .metadata(COIL_HEAT, 49999)
             .addTo(godforgeMoltenRecipes);
+
+        GTValues.RA.stdBuilder()
+            .itemInputsUnsafe(
+                GTUtility.copyAmount(0, CustomItemList.Machine_Multi_EyeOfHarmony.get(1)),
+                GTUtility.copyAmountUnsafe(Integer.MAX_VALUE,MaterialsUEVplus.MagMatter.getDust(1)),
+                GTUtility.copyAmountUnsafe(Integer.MAX_VALUE,GTOreDictUnificator.get(OrePrefixes.dust, Materials.Bedrockium, 1)),
+                GTUtility.copyAmountUnsafe(Integer.MAX_VALUE,MaterialsUEVplus.Eternity.getDust(1)),
+                GTUtility.copyAmountUnsafe(Integer.MAX_VALUE,Materials.Infinity.getDust(1))
+
+            )
+            .fluidInputs(
+                MaterialsUEVplus.Antimatter.getFluid(1919810),
+                MaterialsUEVplus.PhononMedium.getFluid(1145140)
+            )
+            .itemOutputs(
+                EOHBCatalyst.get(OrePrefixes.dust, 1)
+            )
+            .eut(TierEU.RECIPE_UXV)
+            .duration(3000 * SECONDS)
+            .addTo(mixerNonCellRecipes);
     }
 }
