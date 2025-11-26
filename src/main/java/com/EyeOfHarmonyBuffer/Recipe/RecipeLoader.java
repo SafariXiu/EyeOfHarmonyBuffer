e972349cd7f23ac5e83cc64c9a4215944065aae6
Safari_Xiu <xiusafari@gmail.com>
2025-11-26 20:47:16 +0800

    为蓝狗喷泉主方块添加配方

--- src/main/java/com/EyeOfHarmonyBuffer/Recipe/RecipeLoader.java
+++ src/main/java/com/EyeOfHarmonyBuffer/Recipe/RecipeLoader.java
@@ -4,6 +4,8 @@ import com.EyeOfHarmonyBuffer.common.GTCMItemList;
 import com.EyeOfHarmonyBuffer.utils.IRecipePool;
 import cpw.mods.fml.common.registry.GameRegistry;
 import fox.spiteful.avaritia.crafting.ExtremeCraftingManager;
+import gregtech.api.enums.ItemList;
+import gregtech.api.enums.Materials;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -39,7 +41,7 @@ public class RecipeLoader {
         );*/
 
         ExtremeCraftingManager.getInstance().addExtremeShapedOreRecipe(
-            new ItemStack(GTCMItemList.ChengDuHeart.getItem(),1),
+            GTCMItemList.ChengDuHeart.get(1),
             "..AAAAA..",
             "..BBBBB..",
             "...CCC...",
@@ -57,6 +59,18 @@ public class RecipeLoader {
             'F', getModItem(IndustrialCraft2.ID, "itemPartIndustrialDiamond",1),
             'G', getModItem(Botania.ID,"manaResource",1,2),
             'H', getModItem(ExtraUtilities.ID, "spike_base_diamond",1)
+        );
+
+        GameRegistry.addRecipe(
+            GTCMItemList.BlueDogMachines.get(1),
+            "AAA",
+            "BCB",
+            "DED",
+            'A', getModItem(Chisel.ID, "hempcrete",1,0),
+            'B', Materials.Water.getCells(1),
+            'C', ItemList.Hull_LV.get(1),
+            'D', getModItem(Chisel.ID, "hempcrete",1,11),
+            'E', GTCMItemList.ChengDuHeart.get(1)
         );
     }
 }
