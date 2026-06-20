26e701a7728b6b58c2e262b49875f41fee47647b
Safari_Xiu <xiusafari@gmail.com>
2026-06-21 02:33:42 +0800

    适配290

--- src/main/java/com/EyeOfHarmonyBuffer/Mixins/OthTech/MegaNineInOneMixin.java
+++ src/main/java/com/EyeOfHarmonyBuffer/Mixins/OthTech/MegaNineInOneMixin.java
@@ -15,7 +15,6 @@ import gregtech.api.util.ParallelHelper;
 import net.minecraft.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
