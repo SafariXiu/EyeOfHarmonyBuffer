416ae24e961c555c861a02427fcee4cd73d12d92
Safari_Xiu <xiusafari@gmail.com>
2026-06-25 19:21:48 +0800

    添加剩余几种矿脉块

---
+++ src/main/java/com/EyeOfHarmonyBuffer/common/Block/Arknights/BlockLanTieMain.java
@@ -0,0 +1,55 @@
+package com.EyeOfHarmonyBuffer.common.Block.Arknights;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+
+public class BlockLanTieMain extends Block {
+
+    public BlockLanTieMain() {
+        super(Material.glass);
+
+        this.setBlockUnbreakable();
+        this.setResistance(6000000.0F);
+        this.setStepSound(soundTypeGlass);
+        this.setLightOpacity(0);
+        this.setLightLevel(0.8F);
+    }
+
+    @Override
+    public boolean isOpaqueCube() {
+        return false;
+    }
+
+    @Override
+    public int getRenderBlockPass() {
+        return 1;
+    }
+
+    @Override
+    public boolean renderAsNormalBlock() {
+        return true;
+    }
+
+    @Override
+    public int quantityDropped(java.util.Random random) {
+        return 0;
+    }
+
+    @Override
+    public Item getItemDropped(int meta, java.util.Random random, int fortune) {
+        return null;
+    }
+
+    @Override
+    protected boolean canSilkHarvest() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldSideBeRendered(net.minecraft.world.IBlockAccess world,
+                                        int x, int y, int z, int side) {
+        Block block = world.getBlock(x, y, z);
+        return block != this && super.shouldSideBeRendered(world, x, y, z, side);
+    }
+}
--- src/main/java/com/EyeOfHarmonyBuffer/common/Block/ArknightsBlockRegister.java
+++ src/main/java/com/EyeOfHarmonyBuffer/common/Block/ArknightsBlockRegister.java
@@ -1,8 +1,6 @@
 package com.EyeOfHarmonyBuffer.common.Block;
 
-import com.EyeOfHarmonyBuffer.common.Block.Arknights.BlcokOverdomainErosion;
-import com.EyeOfHarmonyBuffer.common.Block.Arknights.BlockYuanShi;
-import com.EyeOfHarmonyBuffer.common.Block.Arknights.BlockYuanShiMain;
+import com.EyeOfHarmonyBuffer.common.Block.Arknights.*;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 
@@ -13,6 +11,12 @@ public class ArknightsBlockRegister {
     public static Block YuanShiMainBlock;
     public static Block YuanShiBlock;
     public static Block OverdomainErosion;
+    public static Block LanTieBlock;
+    public static Block LanTieMainBlock;
+    public static Block ZiJinghBlock;
+    public static Block ZiJingMainBlock;
+    public static Block ChiTongBlock;
+    public static Block ChiTongMainBlock;
 
     public static void registryBlocks(){
 
@@ -33,6 +37,42 @@ public class ArknightsBlockRegister {
             .setBlockTextureName("eyeofharmonybuffer:Arknights/overdomain_erosion_portal")
             .setCreativeTab(ArknightsProject_Block);
         GameRegistry.registerBlock(OverdomainErosion, "overdomain_erosion");
+
+        LanTieBlock = new BlockLanTie()
+            .setBlockName("lan_tie_block")
+            .setBlockTextureName("eyeofharmonybuffer:Arknights/lantie_block")
+            .setCreativeTab(ArknightsProject_Block);
+        GameRegistry.registerBlock(LanTieBlock, "lan_tie_block");
+
+        LanTieMainBlock = new BlockLanTieMain()
+            .setBlockName("lan_tie_main_block")
+            .setBlockTextureName("eyeofharmonybuffer:Arknights/lantie_main_block")
+            .setCreativeTab(ArknightsProject_Block);
+        GameRegistry.registerBlock(LanTieMainBlock, "lan_tie_main_block");
+
+        ZiJinghBlock = new BlockZiJing()
+            .setBlockName("zi_jing_block")
+            .setBlockTextureName("eyeofharmonybuffer:Arknights/zijing_block")
+            .setCreativeTab(ArknightsProject_Block);
+        GameRegistry.registerBlock(ZiJinghBlock, "zi_jing_block");
+
+        ZiJingMainBlock = new BlockZiJingMain()
+            .setBlockName("zi_jing_main_block")
+            .setBlockTextureName("eyeofharmonybuffer:Arknights/zijing_main_block")
+            .setCreativeTab(ArknightsProject_Block);
+        GameRegistry.registerBlock(ZiJingMainBlock, "zi_jing_main_block");
+
+        ChiTongBlock = new BlockZiJing()
+            .setBlockName("chi_tong_block")
+            .setBlockTextureName("eyeofharmonybuffer:Arknights/chitong_block")
+            .setCreativeTab(ArknightsProject_Block);
+        GameRegistry.registerBlock(ChiTongBlock, "chi_tong_block");
+
+        ChiTongMainBlock = new BlockZiJingMain()
+            .setBlockName("chi_tong_main_block")
+            .setBlockTextureName("eyeofharmonybuffer:Arknights/chitong_main_block")
+            .setCreativeTab(ArknightsProject_Block);
+        GameRegistry.registerBlock(ChiTongMainBlock, "chi_tong_main_block");
     }
 
     public static void registry() {
